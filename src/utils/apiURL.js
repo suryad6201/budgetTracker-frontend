5059039cd567620afcc94f74bea445ea9209064c
Suryadhanasekar <suryad6201@gmail.com>
2023-04-17 21:26:34 +0530

    changes

--- src/utils/apiURL.js
+++ src/utils/apiURL.js
@@ -1,3 +1,6 @@
-export const API_URL_USER = "http://localhost:9000/api/v1/users";
-export const API_URL_ACCOUNT = "http://localhost:9000/api/v1/accounts";
-export const API_URL_TRANSACTION = "http://localhost:9000/api/v1/transactions";
+export const API_URL_USER =
+  "https://budget-tracker-backend.onrender.com/api/v1/users";
+export const API_URL_ACCOUNT =
+  "https://budget-tracker-backend.onrender.com/api/v1/accounts";
+export const API_URL_TRANSACTION =
+  "https://budget-tracker-backend.onrender.com/api/v1/transactions";
